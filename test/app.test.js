4da3a26a99b3d928203a993c94164ef89d60064b
Enrique Montemayor <eemontemayor@live.com>
2019-04-02 16:29:25 -0500

    added test for genres query string

--- test/app.test.js
+++ test/app.test.js
@@ -15,4 +15,22 @@ describe('Playstore App', () => {
         expect(res.body).to.have.lengthOf(store.length);
       });
   })
+  it('should filter by genre', ()=>{
+    return request(app)
+      .get('/app')
+      // set up an array of genres names.. loop through that
+      //and input [x] into where genre name goes
+      .query({genres:'Card'})
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        for(let x=0; x<res.body.length; x++){
+          expect(res.body[x].Genres).to.equal('Card')
+        }
+       
+
+      })
+  })
+
 });
